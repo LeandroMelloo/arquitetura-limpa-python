dfed0d8e66a9361bcb6f9557de5ef08c8f1d871d
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-05-26 11:14:51 -0300

    Criando o dashboard co streamlit

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# # dataframe
-# st.dataframe(df)
-
-# # gráfico de linha
-# st.line_chart(df)
-
-# # gráfico de barras
-# st.bar_chart(df)
-
 if st.sidebar.button('Exibir gráfico'):
     df = pd.DataFrame(
         np.random.rand(10, 4),
